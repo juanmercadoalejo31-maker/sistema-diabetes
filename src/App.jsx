dfad3f396699037d20be63dc2f72bd540c1f5537
Juan ABraham Mercado Alejo <juanmercadoalejo31@gmail.com>
2026-05-19 10:05:54 -0600

    Actualizacion del crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/Home/HomePage"
 import Equipo from "./pages/Equipo/Equipo"
 import Crud from "./pages/crud/Crud"
